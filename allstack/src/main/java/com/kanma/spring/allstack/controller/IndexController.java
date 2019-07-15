17018b4e32357a039d725f5c7f0b419715274403
carryma <kanma.cn@gmail.com>
2019-07-16 00:55:14 +0800

    get table datas from ajax using vue

--- allstack/src/main/java/com/kanma/spring/allstack/controller/IndexController.java
+++ allstack/src/main/java/com/kanma/spring/allstack/controller/IndexController.java
@@ -4,6 +4,9 @@ import com.kanma.spring.allstack.entity.UserInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @ Author ：Ma Kang
  * @ Date   ：Created on 2019/7/14 18:39
@@ -13,15 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping("user/info")
-    public UserInfo index() {
+    public List<UserInfo> index() {
 
-        UserInfo userInfo = new UserInfo();
-        userInfo.setName("mk");
-        userInfo.setTel("17625759149");
-        userInfo.setHobby("唱, 跳, rap, 篮球, Music...");
-        userInfo.setAddress("上海携程计算机有限公司");
+        List<UserInfo> userInfoList = new ArrayList<>();
 
-        return userInfo;
+        for (int i = 0; i < 10; i++) {
+            UserInfo userInfo = new UserInfo();
+            userInfo.setName("mk");
+            userInfo.setTel("17625759149");
+            userInfo.setHobby("唱, 跳, rap, 篮球, Music...");
+            userInfo.setAddress("上海携程计算机有限公司");
+            userInfoList.add(userInfo);
+        }
+
+
+        return userInfoList;
     }
 
 }
